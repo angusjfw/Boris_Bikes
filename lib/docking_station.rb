b873b9ea62287053b51743d42c117b38cae4c386
vegim qehaja <gimi-q@vegims-MacBook-Pro.local>
2015-11-24 12:44:58 +0000

    added dock method to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
+require './lib/bike.rb'
+
 class DockingStation
 
-  def release_bike 
+  def release_bike
+    Bike.new
+  end
+
+  def dock(bike)
+
 
   end
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,5 +5,9 @@ describe DockingStation do
   it 'responds to release_bike message' do
     expect(subject.respond_to?(:release_bike)).to eq(true)
   end
-  
+
+  it 'can bike be docked' do
+    expect(subject.respond_to?(:dock)).to eq(true)
+  end
+
 end
